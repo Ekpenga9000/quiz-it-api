968ebb897c8cb1f9dd397257a730a3a180038d16
Louis_Ekpenga <ekpenga9000@gmail.com>
2023-08-11 12:40:53 -0400

    Cors Refactor

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ require('dotenv').config();
 const quizRoute = require("./routes/quiz");
 const port = process.env.PORT; 
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
-
 app.use("/quiz", quizRoute);
 
 app.listen(port, () => {
